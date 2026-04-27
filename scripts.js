2487eb39dfe0893a33d60f7d1a34a4756b55ffc9
anthonykuma25 <anthonykuma2022@gmail.com>
2026-04-27 14:23:15 +0000

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,37 @@
+function payroll(){
+    // declare variables
+    const name = "Anthony Kuma Batsa";
+    const role = "Manager";
+    const baseSalary = 23000;
+    const hoursWorked = 190;// total hours worked in the month (including overtime) = 45 * 4 + 10   
+    
+    //Calculation for Overtime
+    if (hoursWorked > 180) {
+    let extraHours = hoursWorked - 180;
+    overtime = extraHours * (0.02 * baseSalary);
+    }
+
+    //Bonus Calculation
+    if (role === "Manager") {
+    bonus = 0.05 * (baseSalary + overtime);
+    } 
+    else if (role === "C-Level") {
+    bonus = 0.08 * (baseSalary + overtime);
+    } 
+    else if (role === "Sales") {
+    bonus = 0.04 * sales;
+    } 
+
+    gross = baseSalary + overtime + bonus;
+    pension = 0.12 * gross;
+    tax = gross * 0.1; 
+    net = gross - (pension + tax);
+    
+    console.log("Employee Name: " + name);
+    console.log("Role: " + role);
+    console.log("Base Salary: GHS " + baseSalary);
+    console.log("Net Salary: GHS " + net);
+
+}
+
+payroll();
